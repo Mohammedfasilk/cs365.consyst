8821e0322b1ac1d050a3081c5089333e1c246213
mohammed fasil <fmohammedk@consyst.biz>
2025-06-13 14:57:08 +0530

    first commit

--- Server-side/Server.js
+++ Server-side/Server.js
@@ -44,9 +44,9 @@ app.use('/api/signature', Signature);
 app.use('/api/projects',Project);
 
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT,()=>{
-    console.log("running at",PORT);
+
+app.listen(3001,()=>{
+    console.log("running");
     
 })
 // module.exports = app;
