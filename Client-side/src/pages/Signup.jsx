7f66c97fcc33d5cbba1329419c38e8f51feae9c1
mohammed fasil <fmohammedk@consyst.biz>
2025-06-13 15:31:29 +0530

    first commit

--- Client-side/src/pages/Signup.jsx
+++ Client-side/src/pages/Signup.jsx
@@ -1,18 +1,9 @@
 
-import { useIsAuthenticated } from '@azure/msal-react';
 import SignInButton from '../components/UI/SignInButton'
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+
 
 function Signup() {
-  const isAuthenticated = useIsAuthenticated();
-  const navigate = useNavigate();
 
-  useEffect(() => {
-    if (isAuthenticated) {
-      navigate("/home");
-    }
-  }, [isAuthenticated, navigate]);
   
   return (
     <div className='flex flex-col h-screen w-screen justify-center items-center bg-blue-100/50'>
