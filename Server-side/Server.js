d227585e2a8c52f696c243a366b12f47345f6d35
mohammed fasil <fmohammedk@consyst.biz>
2025-06-20 17:32:38 +0530

    first commit

--- Server-side/Server.js
+++ Server-side/Server.js
@@ -21,7 +21,7 @@ const costControl = require('./Routes/costControlRoute')
 //middlewares
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://cs365consyst.vercel.app',
     methods: 'GET,POST',
   }))
 
