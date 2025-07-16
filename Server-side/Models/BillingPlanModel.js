cfb5e348401a11776a47a309b8804c8e26814325
mohammed fasil <fmohammedk@consyst.biz>
2025-07-16 18:36:27 +0530

    billing plans

--- Server-side/Models/BillingPlanModel.js
+++ Server-side/Models/BillingPlanModel.js
@@ -1,24 +1,24 @@
-// models/BillingPlan.js
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const entrySchema = new mongoose.Schema({
   date: { type: String, required: true },
-  description: { type: String},
-  amount: { type: Number, required: true , default:0 },
+  description: { type: String },
+  amount: { type: Number, required: true, default: 0 },
   status: { type: String, enum: ["draft", "approved"], default: "draft" },
 });
 
-const financeSchema = new mongoose.Schema({
+const billingPlanSchema = new mongoose.Schema({
   salesOrderName: { type: String, required: true },
-  currency: { type: String, enum: ["INR", "USD"], required: true },
-  salesOrderValue: { type: Number, required: true },
-  company: { type: String},
-  customerName: { type: String},
-  salesOrderDate: { type: String},
-  country: { type: String},
-  adjustment: { type: Number},
-  adjustedSalesValueUsd: { type: Number,default:0},
-  adjustedSalesValue: { type: Number,default:0},
+  currency: { type: String },
+  salesOrderValue: { type: Number },
+  company: { type: String },
+  customerName: { type: String },
+  salesOrderDate: { type: String },
+  country: { type: String },
+  adjustment: { type: Number },
+  adjustedSalesValueUsd: { type: Number, default: 0 },
+  adjustedSalesValue: { type: Number, default: 0 },
   billing_plans: [entrySchema],
 });
-mongoose.model("Finance", financeSchema);
+
+module.exports = mongoose.model("BillingPlan", billingPlanSchema);
